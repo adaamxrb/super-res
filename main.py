3f02b803fdf303973895133bfb2a1ea9a86124b5
adaamxrb <adamroybafiel@gmail.com>
2024-12-23 19:34:34 +0700

    fixed some bugs

--- main.py
+++ main.py
@@ -15,27 +15,26 @@ import time
 # ============================
 # 🎯 Konfigurasi Pelatihan
 # ============================
-parser = argparse.ArgumentParser(description='Contoh Penggunaan')
-parser.add_argument('--upscale_factor', type=int, default=2, help="upscale factor")
-parser.add_argument('--batchSize', type=int, default=2, help='jumlah batch')
-parser.add_argument('--epochs', type=int, default=20, help='jumlah epoch')
-parser.add_argument('--snapshots', type=int, default=11, help='interval snapshot')
-parser.add_argument('--start_iter', type=int, default=1, help='dimulainya epoch')
-parser.add_argument('--lr', type=float, default=1e-4, help='jumlah Learning Rate. Default=0.0001')
-parser.add_argument('--gpu_mode', type=bool, default=True)
-parser.add_argument('--threads', type=int, default=4, help='jumlah thread data loader')
-parser.add_argument('--seed', type=int, default=123, help='random seed yang digunakan')
-parser.add_argument('--gpus', default=1, type=int, help='jumlah gpu')
-parser.add_argument('--data_dir', type=str, default='./Dataset')
-parser.add_argument('--data_augmentation', type=bool, default=True)
-parser.add_argument('--hr_train_dataset', type=str, default='DIV2K_train_HR')
-parser.add_argument('--model_type', type=str, default='DBPN')
-parser.add_argument('--residual', type=bool, default=True)
-parser.add_argument('--patch_size', type=int, default=40,help='ukuran gambar high resolution yang di-crop')
-parser.add_argument('--pretrained_sr', default='pt_model/DBPN_2x.pth', help='lokasi pretrained model base sr')
-parser.add_argument('--pretrained', type=bool, default=True)
+parser = argparse.ArgumentParser(description='Contoh Penggunaan : \npython main.py --upscale_factor 2 --batchSize 4 --epochs 200 --snapshots 10 --start_iter 1 --lr 0.01 --gpu_mode True --threads 4 --seed 123 --gpus 1 --data_dir ./Dataset --data_augmentation True --hr_train_dataset DIV2K_train_HR --residual True --patch_size 40 --pretrained_sr pt_model/DBPN_2x.pth --pretrained True --save_folder tr_model/ --prefix -x2-residual')
+parser.add_argument('--upscale_factor', type=int, default=4, help="Faktor upscaling")
+parser.add_argument('--batchSize', type=int, default=2, help='Ukuran batch')
+parser.add_argument('--epochs', type=int, default=50, help='Jumlah epoch')
+parser.add_argument('--snapshots', type=int, default=10, help='Interval Checkpoint')
+parser.add_argument('--start_iter', type=int, default=1, help='Mulai iterasi')
+parser.add_argument('--lr', type=float, default=1e-4, help='Jumlah Learning Rate. Default=0.0001')
+parser.add_argument('--gpu_mode', type=bool, default=True, help='Apakah menggunakan GPU')
+parser.add_argument('--threads', type=int, default=4, help='Jumlah thread data loader')
+parser.add_argument('--seed', type=int, default=123, help='Random seed yang digunakan')
+parser.add_argument('--gpus', default=1, type=int, help='Jumlah gpu yang digunakan')
+parser.add_argument('--data_dir', type=str, default='./Dataset' , help='lokasi dataset')
+parser.add_argument('--data_augmentation', type=bool, default=True, help='Apakah menggunakan data augmentation')
+parser.add_argument('--hr_train_dataset', type=str, default='DIV2K_train_HR', help='Nama dataset high resolution')
+parser.add_argument('--residual', type=bool, default=True, help='Apakah menggunakan residual learning')
+parser.add_argument('--patch_size', type=int, default=40,help='Ukuran gambar high resolution yang di-crop')
+parser.add_argument('--pretrained_sr', default='pt_model/DBPN_4x.pth', help='Lokasi pretrained model base sr')
+parser.add_argument('--pretrained', type=bool, default=True, help='Apakah menggunakan pretrained model')
 parser.add_argument('--save_folder', default='tr_model/', help='Lokasi penyimpanan model checkpoint')
-parser.add_argument('--prefix', default='-x2-residual', help='jenis model yang disimpan')
+parser.add_argument('--prefix', default='-x4-residual-dbpn', help='Jenis model yang disimpan')
 
 opt = parser.parse_args()
 gpus_list = range(opt.gpus)
@@ -100,7 +99,7 @@ def test(epoch):
         psnr = 10 * log10(1 / mse.item())
         avg_psnr += psnr
 
-        print("===> Pengujian Epoch[{}]({}/{})  : 🎯  PSNR: {:.4f} dB".format(
+        print("===> Melakukan Tes [{}]({}/{})  : 🎯  PSNR: {:.4f} dB".format(
             epoch, iteration, len(testing_data_loader), psnr))
 
     avg_psnr = avg_psnr / len(testing_data_loader)
@@ -113,7 +112,7 @@ def test(epoch):
 
 def checkpoint(epoch):
     model_out_path = opt.save_folder + opt.hr_train_dataset + "-" + \
-        opt.model_type + opt.prefix + "_epoch_{}.pth".format(epoch)
+        "DBPN" + opt.prefix + "_epoch_{}.pth".format(epoch)
     torch.save(model.state_dict(), model_out_path)
     print("Checkpoint disimpan di: {}".format(model_out_path))
 
